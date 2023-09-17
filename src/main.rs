7de7b31baef2cb9a9b314d0ed22752bc24f2ec32
0x1DEA <seebeyondedm@gmail.com>
2023-09-17 00:40:56 -0500

    prototype 3

--- src/main.rs
+++ src/main.rs
@@ -1,110 +1,147 @@
-use std::collections::HashMap;
 use std::{env, fs};
+use std::time::Instant;
 
-struct Object {
-    id: u32,    // 1
-    x: f32,     // 2
-    y: f32,     // 3
-}
-
-fn get_key_type(key: &str) -> Option<&'static str> {
-
-    let type_lookup: HashMap<&str, &'static str> = [
-        ("1", "u32"),
-        ("2", "f32"),
-        ("3", "f32"),
-        ("4", "bool"),
-        ("5", "bool"),
-        ("6", "float"),
-        ("7", "u8"), //
-        ("8", "u8"), // FIXME: u8 colors rgb
-        ("9", "u8"), //
-        ("10", "f32"),
-        ("11", "bool"),
-        ("12", "u8"), //  FIXME: Secret coin id maybe u8
-        ("13", "bool"),
-        ("14", "bool"),
-        ("15", "bool"),
-        ("16", "bool"),
-        ("17", "bool"),
-        //("18", "f32"),
-        ("19", "u16"), // FIXME: Group ID
-        ("20", "i32"),
-        ("21", "u32"),
-        ("22", "u16"), // FIXME: Group ID
-        ("23", "u16"), // FIXME: Group ID
-        ("24", "i32"),
-        ("25", "i32"),
-        //("26", "f32"),
-        //("27", "f32"),
-        ("28", "f32"),
-        ("29", "f32"),
-        //("30", "f32"), // FIXME: Easing
-        ("31", "string:b64"),
-        ("32", "f32"),
-        ("33", "u16"), // FIXME: Group ID
-        ("34", "bool"),
-        ("35", "f32"),
-        //("36", "f32"),
-        //("37", "f32"),
-        //("38", "f32"),
-        //("39", "f32"),
-        //("40", "f32"),
-        ("41", "bool"),
-        ("42", "bool"),
-        //("43", "bool"), // FIXME: HSV
-        //("44", "bool"), // FIXME: HSV
-        ("45", "f32"),
-        ("46", "f32"),
-        ("47", "f32"),
-        //("48", "bool"), // FIXME: Pulse mode
-        //("49", "bool"), // FIXME: HSV
-        ("50", "u16"), // FIXME: Group ID
-        ("51", "u16"), // FIXME: Group ID
-        //("52", "u32"), // FIXME: Pulse Type
-        ("54", "f32"),
-    ]
-        .iter()
-        .cloned()
-        .collect();
-
-    type_lookup.get(key).cloned()
-}
-
-enum Type {
-    Int(i32),
-    UnsignedInt(u32),
-    Float(f32),
-    String(String),
-    Bool(bool)
+enum Value {
+    UnsignedInt8(),
+    UnsignedInt16(),
+    UnsignedInt32(),
+    SignedInt32(),
+    Bool(),
+    Float(),
+    String(),
+    StringB64(),
+    None(),
 }
 
 fn main() {
-    println!("keyed2gml");
-    // Read in file
-    let data = fs::read_to_string(&env::args().nth(1).unwrap()).expect("Unable to read file");
-
-    // Split into vector
-    let objects_raw: Vec<&str> = data.split(';').collect();
-
-    let mut objects: Vec<Object> = vec![];
-
+    let start = Instant::now();
     let mut skipped: u32 = 0;
-
     let mut bytes: Vec<u8> = vec![];
     bytes.extend_from_slice("GDLVL".as_bytes());
+    println!("Working...");
+
+    // <editor-fold Lookup Table>
+    let type_lookup: [Value; 109] = [
+        Value::None(), // key starts at 1, this will never be used
+        Value::UnsignedInt16(),
+        Value::Float(),
+        Value::Float(),
+        Value::Bool(),
+        Value::Bool(),
+        Value::Float(),
+        Value::UnsignedInt8(),
+        Value::UnsignedInt8(),
+        Value::UnsignedInt8(),
+        Value::Float(),
+        Value::Bool(),
+        Value::UnsignedInt8(),
+        Value::Bool(),
+        Value::Bool(),
+        Value::Bool(),
+        Value::Bool(),
+        Value::Bool(),
+        Value::None(),
+        Value::UnsignedInt16(),
+        Value::SignedInt32(),
+        Value::UnsignedInt32(),
+        Value::UnsignedInt16(),
+        Value::UnsignedInt16(),
+        Value::SignedInt32(),
+        Value::SignedInt32(),
+        Value::None(),
+        Value::None(),
+        Value::Float(),
+        Value::Float(),
+        Value::None(), //"easing",
+        Value::StringB64(), //"string:b64",
+        Value::Float(),
+        Value::UnsignedInt16(),
+        Value::Bool(),
+        Value::Float(),
+        Value::None(),
+        Value::None(),
+        Value::None(),
+        Value::None(),
+        Value::None(),
+        Value::Bool(),
+        Value::Bool(),
+        Value::None(), //"hsv",
+        Value::None(), //"hsv",
+        Value::Float(),
+        Value::Float(),
+        Value::Float(),
+        Value::None(), //"pulse_mode",
+        Value::None(), //"hsv",
+        Value::UnsignedInt16(),
+        Value::UnsignedInt16(),
+        Value::None(), //"pulse_type",
+        Value::None(),
+        Value::Float(),
+        Value::Bool(),
+        Value::Bool(),
+        Value::None(), //"integer_array",
+        Value::Bool(),
+        Value::Bool(),
+        Value::Bool(),
+        Value::SignedInt32(),
+        Value::Bool(),
+        Value::Float(),
+        Value::Bool(),
+        Value::Bool(),
+        Value::Bool(),
+        Value::Bool(),
+        Value::SignedInt32(),
+        Value::SignedInt32(),
+        Value::Bool(),
+        Value::UnsignedInt16(),
+        Value::Float(),
+        Value::None(),
+        Value::Float(),
+        Value::Float(),
+        Value::UnsignedInt16(),
+        Value::SignedInt32(),
+        Value::Bool(),
+        Value::None(), //"pickup_item_mode",
+        Value::UnsignedInt32(),
+        Value::Bool(),
+        Value::None(), //"touch_toggle_mode",
+        Value::None(),
+        Value::Float(),
+        Value::Float(),
+        Value::Bool(),
+        Value::Bool(),
+        Value::None(), //"instant_count_comparison",
+        Value::Bool(),
+        Value::Float(),
+        Value::Float(),
+        Value::Float(),
+        Value::Bool(),
+        Value::Bool(),
+        Value::UnsignedInt16(),
+        Value::Bool(),
+        Value::Float(),
+        Value::Bool(),
+        Value::Bool(),
+        Value::Bool(),
+        Value::None(), //"target_pos_coordinates",
+        Value::Bool(),
+        Value::Bool(),
+        Value::Bool(),
+        Value::Float(),
+        Value::Bool(),
+        Value::Float(),
+        Value::UnsignedInt16(),
+    ];
+    // </editor-fold>
+
+    let data = fs::read_to_string(&env::args().nth(1).unwrap()).expect("Unable to read file");
+    let objects_raw: Vec<&str> = data.split(';').collect();
 
     // First object (0) is special header
     let mut i = 1;
     'objects: while i < objects_raw.len() {
-        if i % 10000 == 0 {
-            println!("Object {}/{}", i / 2, objects_raw.len() / 2);
-        }
         // Collect key:value pairs in a single object string
         let parts: Vec<&str> = objects_raw[i].split(',').collect();
-
-        // HashMap for single object's keys and values
-        let mut map: HashMap<u32, Type> = HashMap::new();
 
         let mut obj_index = 0;
         while obj_index < parts.len() {
@@ -113,41 +150,51 @@ fn main() {
                 continue 'objects;
             }
 
-            let key = parts[obj_index].to_string().parse::<u32>().unwrap();
+            let key = parts[obj_index].parse::<u8>().unwrap();
             obj_index += 1;
 
             // If there is a value to the current key, we go by 2's remember
             if obj_index < parts.len() {
                 let value = parts[obj_index].to_string();
 
-                match get_key_type(&key.to_string()) {
-                    Some("string") => {
-                        //map.insert(key, Type::String(value));
-                    }
-                    Some("string:b64") => {
-                        //
-                    }
-                    Some("i32") => {
+                match type_lookup[key as usize] {
+                    Value::String() => bytes.extend_from_slice(value.as_bytes()),
+                    Value::StringB64() => bytes.extend_from_slice(value.as_bytes()),
+                    Value::SignedInt32() => {
                         if let Ok(parsed_value) = value.parse::<i32>() {
-                            //map.insert(key, Type::Int(parsed_value));
                             bytes.extend_from_slice(parsed_value.to_le_bytes().as_slice());
                         }
                     }
-                    Some("u32") => {
+                    Value::UnsignedInt32() => {
                         if let Ok(parsed_value) = value.parse::<u32>() {
-                            //map.insert(key, Type::UnsignedInt(parsed_value));
                             bytes.extend_from_slice(parsed_value.to_le_bytes().as_slice());
                         }
                     }
-                    Some("f32") => {
+                    Value::UnsignedInt16() => {
+                        if let Ok(parsed_value) = value.parse::<u16>() {
+                            bytes.extend_from_slice(parsed_value.to_le_bytes().as_slice());
+                        }
+                    }
+                    Value::UnsignedInt8() => {
+                        if let Ok(parsed_value) = value.parse::<u8>() {
+                            bytes.extend_from_slice(parsed_value.to_le_bytes().as_slice());
+                        }
+                    }
+                    Value::Float() => {
                         if let Ok(parsed_value) = value.parse::<f32>() {
-                            //map.insert(key, Type::Float(parsed_value));
                             bytes.extend_from_slice(parsed_value.to_le_bytes().as_slice());
                         }
                     }
-                    _ => {
-                        skipped += 1;
+                    Value::Bool() => {
+                        if let Ok(parsed_value) = value.parse::<bool>() {
+                            if parsed_value {
+                                bytes.push(0x01);
+                            } else {
+                                bytes.push(0x00);
+                            }
+                        }
                     }
+                    _ => skipped += 1
                 }
 
                 // Done, move on to the next key
@@ -155,31 +202,9 @@ fn main() {
             }
         }
 
-        // objects.push(Object {
-        //     id: match map.get(&1) {
-        //         Some(&Type::UnsignedInt(x)) => x,
-        //         _ => panic!("Error: x extraction failed.")
-        //     },
-        //     x: match map.get(&2) {
-        //         Some(&Type::Float(x)) => x,
-        //         _ => panic!("Error: x extraction failed.")
-        //     },
-        //     y: match map.get(&3) {
-        //         Some(&Type::Float(x)) => x,
-        //         _ => panic!("Error: x extraction failed.")
-        //     },
-        // });
-
         i += 1;
     }
 
-    // for object in objects {
-    //     bytes.extend_from_slice(object.id.to_le_bytes().as_slice());
-    //     bytes.extend_from_slice(object.x.to_le_bytes().as_slice());
-    //     bytes.extend_from_slice(object.y.to_le_bytes().as_slice());
-    // }
-
+    println!("Done in {:?}. Skipped {} keys. Writing file...", start.elapsed(), skipped);
     fs::write("out.gdl", bytes).expect("Oppsie poopsie");
-
-    println!("Skipped keys: {}", skipped);
 }
