0c1986b70f53a1477a8774d3135b045fbbb23a60
0x1DEA <seebeyondedm@gmail.com>
2023-09-16 22:17:19 -0500

    prototype 2

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,61 @@ struct Object {
 }
 
 fn get_key_type(key: &str) -> Option<&'static str> {
+
     let type_lookup: HashMap<&str, &'static str> = [
         ("1", "u32"),
         ("2", "f32"),
         ("3", "f32"),
+        ("4", "bool"),
+        ("5", "bool"),
+        ("6", "float"),
+        ("7", "u8"), //
+        ("8", "u8"), // FIXME: u8 colors rgb
+        ("9", "u8"), //
+        ("10", "f32"),
+        ("11", "bool"),
+        ("12", "u8"), //  FIXME: Secret coin id maybe u8
+        ("13", "bool"),
+        ("14", "bool"),
+        ("15", "bool"),
+        ("16", "bool"),
+        ("17", "bool"),
+        //("18", "f32"),
+        ("19", "u16"), // FIXME: Group ID
+        ("20", "i32"),
+        ("21", "u32"),
+        ("22", "u16"), // FIXME: Group ID
+        ("23", "u16"), // FIXME: Group ID
+        ("24", "i32"),
+        ("25", "i32"),
+        //("26", "f32"),
+        //("27", "f32"),
+        ("28", "f32"),
+        ("29", "f32"),
+        //("30", "f32"), // FIXME: Easing
+        ("31", "string:b64"),
+        ("32", "f32"),
+        ("33", "u16"), // FIXME: Group ID
+        ("34", "bool"),
+        ("35", "f32"),
+        //("36", "f32"),
+        //("37", "f32"),
+        //("38", "f32"),
+        //("39", "f32"),
+        //("40", "f32"),
+        ("41", "bool"),
+        ("42", "bool"),
+        //("43", "bool"), // FIXME: HSV
+        //("44", "bool"), // FIXME: HSV
+        ("45", "f32"),
+        ("46", "f32"),
+        ("47", "f32"),
+        //("48", "bool"), // FIXME: Pulse mode
+        //("49", "bool"), // FIXME: HSV
+        ("50", "u16"), // FIXME: Group ID
+        ("51", "u16"), // FIXME: Group ID
+        //("52", "u32"), // FIXME: Pulse Type
+        ("54", "f32"),
     ]
         .iter()
         .cloned()
@@ -25,6 +76,7 @@ enum Type {
     UnsignedInt(u32),
     Float(f32),
     String(String),
+    Bool(bool)
 }
 
 fn main() {
@@ -38,6 +90,9 @@ fn main() {
     let mut objects: Vec<Object> = vec![];
 
     let mut skipped: u32 = 0;
+
+    let mut bytes: Vec<u8> = vec![];
+    bytes.extend_from_slice("GDLVL".as_bytes());
 
     // First object (0) is special header
     let mut i = 1;
@@ -67,24 +122,27 @@ fn main() {
 
                 match get_key_type(&key.to_string()) {
                     Some("string") => {
-                        map.insert(key, Type::String(value));
+                        //map.insert(key, Type::String(value));
                     }
                     Some("string:b64") => {
                         //
                     }
                     Some("i32") => {
                         if let Ok(parsed_value) = value.parse::<i32>() {
-                            map.insert(key, Type::Int(parsed_value));
+                            //map.insert(key, Type::Int(parsed_value));
+                            bytes.extend_from_slice(parsed_value.to_le_bytes().as_slice());
                         }
                     }
                     Some("u32") => {
                         if let Ok(parsed_value) = value.parse::<u32>() {
-                            map.insert(key, Type::UnsignedInt(parsed_value));
+                            //map.insert(key, Type::UnsignedInt(parsed_value));
+                            bytes.extend_from_slice(parsed_value.to_le_bytes().as_slice());
                         }
                     }
                     Some("f32") => {
                         if let Ok(parsed_value) = value.parse::<f32>() {
-                            map.insert(key, Type::Float(parsed_value));
+                            //map.insert(key, Type::Float(parsed_value));
+                            bytes.extend_from_slice(parsed_value.to_le_bytes().as_slice());
                         }
                     }
                     _ => {
@@ -97,33 +155,29 @@ fn main() {
             }
         }
 
-        objects.push(Object {
-            id: match map.get(&1) {
-                Some(&Type::UnsignedInt(x)) => x,
-                _ => panic!("Error: x extraction failed.")
-            },
-            x: match map.get(&2) {
-                Some(&Type::Float(x)) => x,
-                _ => panic!("Error: x extraction failed.")
-            },
-            y: match map.get(&3) {
-                Some(&Type::Float(x)) => x,
-                _ => panic!("Error: x extraction failed.")
-            },
-        });
+        // objects.push(Object {
+        //     id: match map.get(&1) {
+        //         Some(&Type::UnsignedInt(x)) => x,
+        //         _ => panic!("Error: x extraction failed.")
+        //     },
+        //     x: match map.get(&2) {
+        //         Some(&Type::Float(x)) => x,
+        //         _ => panic!("Error: x extraction failed.")
+        //     },
+        //     y: match map.get(&3) {
+        //         Some(&Type::Float(x)) => x,
+        //         _ => panic!("Error: x extraction failed.")
+        //     },
+        // });
 
         i += 1;
     }
 
-    let mut bytes: Vec<u8> = vec![];
-
-    bytes.extend_from_slice("GDLVL".as_bytes());
-
-    for object in objects {
-        bytes.extend_from_slice(object.id.to_le_bytes().as_slice());
-        bytes.extend_from_slice(object.x.to_le_bytes().as_slice());
-        bytes.extend_from_slice(object.y.to_le_bytes().as_slice());
-    }
+    // for object in objects {
+    //     bytes.extend_from_slice(object.id.to_le_bytes().as_slice());
+    //     bytes.extend_from_slice(object.x.to_le_bytes().as_slice());
+    //     bytes.extend_from_slice(object.y.to_le_bytes().as_slice());
+    // }
 
     fs::write("out.gdl", bytes).expect("Oppsie poopsie");
 
